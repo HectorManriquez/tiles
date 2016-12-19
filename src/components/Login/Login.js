43818cb1c33130aa148e5a72a47d7b66b463694f
Keitarokido <hector.j.manriquez@gmail.com>
2016-12-19 03:29:16 -0600

    Adding basic template for login react component

---
+++ src/components/Login/Login.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import Head from '../Header/Header';
+
+class Home extends Component {
+    render() {
+        return (
+            <div className="container">
+                <Head/>
+            </div>
+        );
+    }
+}
+
+export default Home;
