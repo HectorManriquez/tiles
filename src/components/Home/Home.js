110d538e57a9be8e8e810e4f2ac192ac5e204535
Keitarokido <hector.j.manriquez@gmail.com>
2016-12-19 17:26:33 -0600

    Set up basic main page for displaying a maximum of 10 users, need to add load more option

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,12 +1,55 @@
 import React, {Component} from 'react';
+import getUsers from '../../utils/getUsers';
 
 class Home extends Component {
+    constructor() {
+        super();
+
+        this.state = null;
+    }
+
+    componentDidMount() {
+        getUsers()
+            .then(function (docs) {
+                this.setState({
+                    users: docs
+                });
+            }.bind(this))
+    }
+
     render() {
-        return (
-            <div>
-                <h1>Home Page</h1>
-            </div>
-        );
+        if (!this.state) {
+            return (
+                <div>
+                    <h1>No Users Registered, be the first one to sign up!</h1>
+                </div>
+            )
+        } else {
+            return (
+                <div>
+                    <h3 className='middle'>All Users</h3>
+                    {this.state.users.map((user, index) => {
+                        return (
+                            <div className='col s12 m6 l6' key={index}>
+                                <h5 className='header'>
+                                    {user.firstName && user.lastName ? user.firstName + ' ' + user.lastName : user.email}
+                                </h5>
+                                <div className='card horizontal'>
+                                    <div className='card-image'>
+                                        <img src={user.picture} height='150px' width='150px'/>
+                                    </div>
+                                    <div className='card-stacked'>
+                                        <div className='card-content'>
+                                            <p>{user.description}</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        )
+                    })}
+                </div>
+            )
+        }
     }
 }
 
