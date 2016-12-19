910487a1cc0762d7b9a26a4c4c2c0a87730f3f2d
Keitarokido <hector.j.manriquez@gmail.com>
2016-12-18 22:07:15 -0600

    Adding a home react component

---
+++ src/components/Home/Home.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import Head from '../Head/Head';
+
+class Home extends Component {
+    render() {
+        return (
+            <div className="container">
+                <Head/>
+            </div>
+        );
+    }
+}
+
+export default Home;
