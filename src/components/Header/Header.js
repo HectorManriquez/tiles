6e1876f057c68091f61600b7b47945a9f96b2ac4
Keitarokido <hector.j.manriquez@gmail.com>
2016-12-19 15:29:52 -0600

    Added a ref to the button collapse a tag so that the button will work correctly when viewing in mobile

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,15 +1,27 @@
 import React, {Component} from 'react';
 
 class Head extends Component {
+    componentDidMount() {
+        $(this.refs.collapseButton).sideNav();
+    }
+
     render() {
         return (
             <nav>
-                <div className="nav-wrapper">
-                    <a href="/#" className="brand-logo">Tiles</a>
-                    <ul id="nav-mobile" className="right hide-on-med-and-down">
-                        <li><a href="/#/dashboard">Dashboard</a></li>
-                        <li><a href="/#/login">Login</a></li>
-                        <li><a href="/#/signup">Sign Up</a></li>
+                <div className='nav-wrapper'>
+                    <a href='/#' className='brand-logo'>Tiles</a>
+                    <a href='#' data-activates='collapse' className='button-collapse right' ref='collapseButton'>
+                        <i className='material-icons'>menu</i>
+                    </a>
+                    <ul className='right hide-on-med-and-down'>
+                        <li><a href='/#/dashboard'>Dashboard</a></li>
+                        <li><a href='/#/login'>Login</a></li>
+                        <li><a href='/#/signup'>Sign Up</a></li>
+                    </ul>
+                    <ul id='collapse' className='side-nav'>
+                        <li><a href='/#/dashboard'>Dashboard</a></li>
+                        <li><a href='/#/login'>Login</a></li>
+                        <li><a href='/#/signup'>Sign Up</a></li>
                     </ul>
                 </div>
             </nav>
