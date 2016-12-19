fd619eb447586c436c8eeb689bc7788969f8edb4
Keitarokido <hector.j.manriquez@gmail.com>
2016-12-19 09:38:54 -0600

    Adding login page to react router

--- src/components/App.js
+++ src/components/App.js
@@ -10,6 +10,7 @@ import {
 import Home from './Home/Home';
 import Dashboard from './Dashboard/Dashboard';
 import Signup from './Signup/Signup';
+import Login from './Login/Login';
 import NotFound from './NotFound';
 
 class App extends Component {
@@ -19,6 +20,7 @@ class App extends Component {
                 <Route path='/' component={Home}/>
                 <Route path='/dashboard' component={Dashboard}/>
                 <Route path='/signup' component={Signup}/>
+                <Route path='/login' component={Login}/>
                 <Route path='*' component={NotFound}/>
             </Router>
         )
