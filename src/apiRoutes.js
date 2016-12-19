1a1096598fa72fb308965e4a9854418710038621
Keitarokido <hector.j.manriquez@gmail.com>
2016-12-19 00:47:08 -0600

    Set up signup post express route

--- src/apiRoutes.js
+++ src/apiRoutes.js
@@ -3,11 +3,44 @@
  */
 
 import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import User from './data/models/User';
+
 const router = express.Router();
 
-router.get("/", function(req, res) {
+router.get('/', function(req, res) {
     // res.sendFile(__dirname + "/public/index.html");
     res.send('API Routes');
+});
+
+//
+// Dashboard routes for viewing and modifying profile
+// -----------------------------------------------------------------------------
+router.get('/userDetails', (req, res) => {
+    res.send('This is some data');
+});
+
+/**
+ * POST to /signup
+ * Creates a user
+ */
+router.post('/signup', (req, res) => {
+    console.log(req.body);
+    User.create({
+        email: req.body.email,
+        password: req.body.password
+    }, (err) => {
+        if (err) {
+            res.send({
+                status: 'failure'
+            });
+        } else {
+            res.send({
+                status: 'success'
+            });
+        }
+    })
 });
 
 export default router;
