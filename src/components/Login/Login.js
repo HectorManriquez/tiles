d02952a842104a9c4de21ccb27bc4a3406e1ff90
Keitarokido <hector.j.manriquez@gmail.com>
2016-12-19 18:11:40 -0600

    Set up login page, needs to hit the database

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,10 +1,35 @@
 import React, {Component} from 'react';
 
 class Home extends Component {
+    handleSubmit(event) {
+        event.preventDefault();
+
+        signupUser({
+            email: document.getElementById('email').value,
+            password: document.getElementById('password').value
+        });
+    }
+
     render() {
         return (
             <div>
-                <h1>Login Page</h1>
+                <form className='col s12 m12 l12' onSubmit={this.handleSubmit}>
+                    <div className='row'>
+                        <div className='input-field col s12 m12 l12'>
+                            <label htmlFor='email'>Email</label>
+                            <input id='email' ref='email' type='text' required/>
+                        </div>
+                    </div>
+                    <div className='row'>
+                        <div className='input-field col s12 m12 l12'>
+                            <label htmlFor='password'>Password</label>
+                            <input id='password' ref='password' type='password' required/>
+                        </div>
+                    </div>
+                    <button className='btn waves-effect waves-light' type='submit' name='login'>
+                        Login
+                    </button>
+                </form>
             </div>
         );
     }
