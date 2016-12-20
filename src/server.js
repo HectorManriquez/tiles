1053b1bf6e99c415b785ed21abb18ee60264c7cc
Keitarokido <hector.j.manriquez@gmail.com>
2016-12-19 23:28:37 -0600

    Comforming to one way of using comments

--- src/server.js
+++ src/server.js
@@ -7,8 +7,6 @@ import path from 'path';
 import express from 'express';
 import bodyParser from 'body-parser';
 import React from 'react';
-// import ReactDom from 'react-dom/server';
-// import schema from './data/models'
 
 import routes from './routes';
 import apiRoutes from './apiRoutes';
@@ -16,22 +14,22 @@ import {port} from './config';
 
 const app = express();
 
-//
-// Register Node.js middleware
-// -----------------------------------------------------------------------------
+/**
+ * Register Node.js middleware
+ */
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-//
-// Register routes in express
-// -----------------------------------------------------------------------------
+/**
+ * Register routes in express
+ */
 app.use('/api', apiRoutes);
 app.use('/', routes);
 
-//
-// Launch the server
-// -----------------------------------------------------------------------------
+/**
+ * Launch the server
+ */
 app.listen(port, () => {
     console.log(`The server is running at http://localhost:${port}/`);
 });
