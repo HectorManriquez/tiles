ee562268975d786ff423e841830862c23e7e11e7
Keitarokido <hector.j.manriquez@gmail.com>
2016-12-19 18:06:03 -0600

    Modifying home page to display all current users, modified the way images should display and the words to adjust according to size

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -27,26 +27,28 @@ class Home extends Component {
         } else {
             return (
                 <div>
-                    <h3 className='middle'>All Users</h3>
-                    {this.state.users.map((user, index) => {
-                        return (
-                            <div className='col s12 m6 l6' key={index}>
-                                <h5 className='header'>
-                                    {user.firstName && user.lastName ? user.firstName + ' ' + user.lastName : user.email}
-                                </h5>
-                                <div className='card horizontal'>
-                                    <div className='card-image'>
-                                        <img src={user.picture} height='150px' width='150px'/>
-                                    </div>
-                                    <div className='card-stacked'>
-                                        <div className='card-content'>
-                                            <p>{user.description}</p>
+                    <h4 className='left-align'>All Current Users</h4>
+                    <div className='row'>
+                        {this.state.users.map((user, index) => {
+                            return (
+                                <div className='col s12 m6 l6' key={index}>
+                                    <h5 className='header flow-text'>
+                                        {user.firstName && user.lastName ? user.firstName + ' ' + user.lastName : user.email}
+                                    </h5>
+                                    <div className='card horizontal'>
+                                        <div>
+                                            <img className='circle' src={user.picture} height='150px' width='150px'/>
+                                        </div>
+                                        <div className='card-stacked'>
+                                            <div className='card-content'>
+                                                <p className='flow-text'>{user.description}</p>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
-                            </div>
-                        )
-                    })}
+                            )
+                        })}
+                    </div>
                 </div>
             )
         }
