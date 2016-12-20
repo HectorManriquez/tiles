e3b6529e6b2aaf6b2673a173bb8ccb6a263373b0
Keitarokido <hector.j.manriquez@gmail.com>
2016-12-20 01:39:59 -0600

    Finished setting up express configuration for using auth through passport

--- src/server.js
+++ src/server.js
@@ -2,24 +2,56 @@
  * Server configuration
  */
 
+/**
+ * Module dependencies
+ */
 import 'babel-polyfill';
 import path from 'path';
 import express from 'express';
+import session from 'express-session';
+import flash from 'express-flash';
+import expressValidator from 'express-validator';
+import compression from 'compression';
 import bodyParser from 'body-parser';
+import passport from 'passport';
 import React from 'react';
+
+const MongoStore = require('connect-mongo')(session);
 
 import routes from './routes';
 import apiRoutes from './apiRoutes';
-import {port} from './config';
+import {
+    port,
+    databaseUrl,
+    sessionSecret
+} from './config';
 
+/**
+ * Create express instance
+ */
 const app = express();
 
 /**
- * Register Node.js middleware
+ * Express configuration
  */
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(compression);
+app.use(expressValidator());
+app.use(session({
+    resave: true,
+    saveUninitialized: true,
+    secret: sessionSecret,
+    store: new MongoStore({
+        url: databaseUrl,
+        autoReconnect: true
+    })
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash);
+
 
 /**
  * Register routes in express
