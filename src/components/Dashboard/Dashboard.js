12255b2ea363bfbdc20ad4eda2b35f22f2958167
Keitarokido <hector.j.manriquez@gmail.com>
2016-12-20 10:30:18 -0600

    Adding get user data helper and populating everything if the user is logged in

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,17 +1,30 @@
 import React, {Component} from 'react';
+import getUser from '../../utils/getUser';
 import updateUser from '../../utils/updateUser';
 import Promise from 'bluebird';
 
 class Dashboard extends Component {
     constructor() {
         super();
-        this.state = {
-            picture: 'http://freevector.co/wp-content/uploads/2014/09/1954-face-with-a-question-mark7.png'
-        };
+
+        this.state = null;
 
         this.handleChange = this.handleChange.bind(this);
         this.handleUpdateSubmit = this.handleUpdateSubmit.bind(this);
         this.handlePasswordSubmit = this.handlePasswordSubmit.bind(this);
+    }
+
+    componentWillMount() {
+        getUser()
+            .then((user) => {
+                if (user) {
+                    this.setState(user);
+                } else {
+                    this.setState({
+                        picture: 'http://freevector.co/wp-content/uploads/2014/09/1954-face-with-a-question-mark7.png'
+                    });
+                }
+            });
     }
 
     handleChange(event) {
@@ -24,7 +37,7 @@ class Dashboard extends Component {
         updateUser(this.state)
             .then((res) => {
                 console.log(res);
-        });
+            });
     }
 
     handlePasswordSubmit(event) {
@@ -33,70 +46,83 @@ class Dashboard extends Component {
     }
 
     render() {
-        return (
-            <div>
-                <h4>Update Information</h4>
-                <form className="col s12 m12 l12" onSubmit={this.handleUpdateSubmit}>
-                    <div className="row">
-                        <div className="col s12 m6 l6">
-                            <img className="responsive-img" width='200px' height='200px'
-                                 src={this.state.picture}/>
-                        </div>
-                        <div className="input-field col s12 m6 l6">
-                            <label htmlFor="picture">Picture link</label>
-                            <input id="picture" type="text" className="validate" onChange={this.handleChange}/>
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="input-field col s12 m12 l12">
-                            <label htmlFor="email">Email</label>
-                            <input id="email" type="text" className="validate" onChange={this.handleChange} required/>
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="input-field col s6 m6 l6">
-                            <label htmlFor="first_name">First Name</label>
-                            <input id="firstName" type="text" className="validate" onChange={this.handleChange}/>
-                        </div>
-                        <div className="input-field col s6 m6 l6">
-                            <label htmlFor="last_name">Last Name</label>
-                            <input id="lastName" type="text" className="validate" onChange={this.handleChange}/>
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="col s12 m12 l12">
-                            <label htmlFor="description">Description of yourself</label>
-                            <div className="input-field inline">
-                                 <textarea id="description" rows="5" cols="25" onChange={this.handleChange}>
-                                 </textarea>
+        if (!this.state) {
+            return <h4>Not allowed</h4>
+        } else {
+            return (
+                <div>
+                    <h4>Update Information</h4>
+                    <form className='col s12 m12 l12' onSubmit={this.handleUpdateSubmit}>
+                        <div className='row'>
+                            <div className='col s12 m6 l6'>
+                                <img className='responsive-img' width='200px' height='200px'
+                                     src={this.state.picture}/>
+                            </div>
+                            <div className='input-field col s12 m6 l6'>
+                                <label htmlFor='picture'>Picture link</label>
+                                <input value={this.state.picture ? this.state.picture : ''}
+                                       id='picture' type='text' className='validate' onChange={this.handleChange}/>
                             </div>
                         </div>
-                    </div>
-                    <button className="btn waves-effect waves-light" type="submit">
-                        Update
-                    </button>
-                </form>
+                        <div className='row'>
+                            <div className='input-field col s12 m12 l12'>
+                                <label htmlFor='email'>Email</label>
+                                <input
+                                    value={this.state.email}
+                                    id='email'
+                                    type='text'
+                                    className='validate'
+                                    onChange={this.handleChange} required/>
+                            </div>
+                        </div>
+                        <div className='row'>
+                            <div className='input-field col s6 m6 l6'>
+                                <label htmlFor='first_name'>First Name</label>
+                                <input value={this.state.firstName ? this.state.firstName : ''}
+                                       id='firstName' type='text' className='validate' onChange={this.handleChange}/>
+                            </div>
+                            <div className='input-field col s6 m6 l6'>
+                                <label htmlFor='last_name'>Last Name</label>
+                                <input value={this.state.lastName ? this.state.lastName : ''}
+                                       id='lastName' type='text' className='validate' onChange={this.handleChange}/>
+                            </div>
+                        </div>
+                        <div className='row'>
+                            <div className='col s12 m12 l12'>
+                                <label htmlFor='description'>Description of yourself</label>
+                                <div className='input-field inline'>
+                                 <textarea value={this.state.description ? this.state.description : ''}
+                                           id='description' rows='5' cols='25' onChange={this.handleChange}>
+                                 </textarea>
+                                </div>
+                            </div>
+                        </div>
+                        <button className='btn waves-effect waves-light' type='submit'>
+                            Update
+                        </button>
+                    </form>
 
-                <h4>Change Password</h4>
-                <form className="col s12 m12 l12" onSubmit={this.handlePasswordSubmit}>
-                    <div className="row">
-                        <div className="input-field col s12 m12 l12">
-                            <label htmlFor="oldPassword">Old Password</label>
-                            <input id="oldPassword" type="password" className="validate"/>
+                    <h4>Change Password</h4>
+                    <form className='col s12 m12 l12' onSubmit={this.handlePasswordSubmit}>
+                        <div className='row'>
+                            <div className='input-field col s12 m12 l12'>
+                                <label htmlFor='oldPassword'>Old Password</label>
+                                <input id='oldPassword' type='password' className='validate'/>
+                            </div>
                         </div>
-                    </div>
-                    <div className="row">
-                        <div className="input-field col s12 m12 l12">
-                            <label htmlFor="newPassword">New Password</label>
-                            <input id="newPassword" type="password" className="validate"/>
+                        <div className='row'>
+                            <div className='input-field col s12 m12 l12'>
+                                <label htmlFor='newPassword'>New Password</label>
+                                <input id='newPassword' type='password' className='validate'/>
+                            </div>
                         </div>
-                    </div>
-                    <button className="btn waves-effect waves-light" type="submit">
-                        Update
-                    </button>
-                </form>
-            </div>
-        )
+                        <button className='btn waves-effect waves-light' type='submit'>
+                            Update
+                        </button>
+                    </form>
+                </div>
+            )
+        }
     }
 }
 
